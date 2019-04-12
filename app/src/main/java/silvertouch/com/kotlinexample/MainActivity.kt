eeebfcb32d03283801a761926f03048a41cd7099
rajan.bhavsar <rajann_2009@yahoo.co.in>
2019-04-12 16:26:04 +0530

    Source committed with Latest Version of Databinding and Kotlin Version

--- app/src/main/java/silvertouch/com/kotlinexample/MainActivity.kt
+++ app/src/main/java/silvertouch/com/kotlinexample/MainActivity.kt
@@ -1,12 +1,16 @@
 package silvertouch.com.kotlinexample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        label.text = "Rajan bhavsar";
     }
 }
