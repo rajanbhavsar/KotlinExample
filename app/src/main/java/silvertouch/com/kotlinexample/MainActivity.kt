e1672a93f2db46a2e5ceb7ce2a01a9d39055d5fc
rajan.bhavsar <rajann_2009@yahoo.co.in>
2019-04-12 17:03:21 +0530

    Databinfing Latest Version Added and tested

--- app/src/main/java/silvertouch/com/kotlinexample/MainActivity.kt
+++ app/src/main/java/silvertouch/com/kotlinexample/MainActivity.kt
@@ -1,16 +1,27 @@
 package silvertouch.com.kotlinexample
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import silvertouch.com.kotlinexample.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var mainBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         label.text = "Rajan bhavsar";
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        Handler().postDelayed({ mainBinding.label.setText("Rajansinh Bhavsar") }, 5000)
     }
 }
