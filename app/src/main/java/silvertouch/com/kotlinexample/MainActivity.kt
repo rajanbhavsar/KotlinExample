97e7eed8f852d87c6dc843c830cc106b6d461351
rajan.bhavsar <rajann_2009@yahoo.co.in>
2019-04-12 16:47:16 +0530

    Removed Databinding

--- app/src/main/java/silvertouch/com/kotlinexample/MainActivity.kt
+++ app/src/main/java/silvertouch/com/kotlinexample/MainActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
         label.text = "Rajan bhavsar";
     }
 }
